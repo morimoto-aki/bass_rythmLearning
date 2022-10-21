e1fc79b6f79943bfc019ba19736ff94f6d658ed8
morimoto-aki <akihiro.morimoto.3v@stu.hosei.ac.jp>
2022-10-22 06:40:37 +0900

    定数別パッケージ化

--- src/file_import.py
+++ src/file_import.py
@@ -1,19 +1,19 @@
 '''全楽譜のファイルのパスを取得するプログラム'''
 import os
+from constant import cons
 
 path_list = []
-PATH = "./music"
-file_list = os.listdir(PATH)
+file_list = os.listdir(cons.PATH)
 # print(fileList)
 
 
 def file_path():
     '''ファイルのパスを取得する関数'''
     for filename in file_list:
-        path_score = PATH + "/" + filename + "/score/"
+        path_score = cons.PATH + "/" + filename + "/score/"
         path_list.append(path_score)
 
     return path_list
 
 
-# print(file_path())
+print(file_path())
